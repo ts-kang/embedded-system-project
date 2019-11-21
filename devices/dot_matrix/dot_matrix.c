1a05c102186a49b1f5b66b973fc0ecbf80dcadd6
Taesu Kang <p@nyan.ch>
2019-11-22 04:57:25 +0900

    dot_matrix: Implement init, destroy function

--- devices/dot_matrix/dot_matrix.c
+++ devices/dot_matrix/dot_matrix.c
@@ -1,1 +1,22 @@
 #include <devices/dot_matrix.h>
+#include <fcntl.h>
+#include <err.h>
+
+#define DEV_PATH	"/dev/fpga_dot_matrix"
+#define ROWS		10
+#define COLS		7
+
+static int dev = -1;
+static uint8_t scr[ROWS];
+
+void dot_matrix_init()
+{
+	dev = open(DEV_PATH, O_WRONLY);
+	if (dev < 0)
+		errx(1, DEV_PATH ": device open failed");
+}
+
+void dot_matrix_destroy()
+{
+	close(dev);
+}
