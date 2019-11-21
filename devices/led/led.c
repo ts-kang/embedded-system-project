42d34fb7422efd77fbda903dcec61679a5a7b659
Taesu Kang <p@nyan.ch>
2019-11-22 05:02:31 +0900

    led: Implement init, destroy function

--- devices/led/led.c
+++ devices/led/led.c
@@ -1,1 +1,19 @@
 #include <devices/led.h>
+#include <fcntl.h>
+#include <err.h>
+
+#define DEV_PATH	"/dev/fpga_led"
+
+static int dev = -1;
+
+void led_init()
+{
+	dev = open(DEV_PATH, O_WRONLY);
+	if (dev < 0)
+		errx(1, DEV_PATH ": device open failed");
+}
+
+void led_destroy()
+{
+	close(dev);
+}
