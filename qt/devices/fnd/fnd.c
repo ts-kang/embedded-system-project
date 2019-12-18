d31d3de12120eeff995c319ab5ed585ef17490ac
jinhak94 <47026766+jinhak94@users.noreply.github.com>
2019-12-18 15:41:05 +0900

    Add files via upload

--- qt/devices/fnd/fnd.c
+++ qt/devices/fnd/fnd.c
@@ -25,91 +25,30 @@ void fnd_init() {
 void fnd_destroy() {
 	close(dev);
 }
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
 
-void *t_function(void *data){
-       int* difficulty = (int*)data;
-        int i = 0;
-
-        if(*difficulty == 3){
-                i = 7000;
-        }else if(*difficulty == 2){
-                i = 5000;
-        }else if(*difficulty == 1){
-                i = 3000;
-
-        };
-
-        unsigned char str[5] = {1,1,1,1};
-
-        while(i>=0){
-               sprintf(str, "%d",i);
-                write(dev,str,4);
-                i--;
-                usleep(1000);
-        }
-        pthread_exit(NULL);
+pthread_t *fnd_thread(int num){
+      int thr_id;
+      pthread_t *p_thread = malloc(sizeof(pthread_t));
+      thr_id = pthread_create(p_thread,NULL,t_function,(void *)num);
+      if(thr_id < 0){
+         perror("thread create error : ");
+         exit(1);
+     }  
+     return &pthread_t;
 }
+ 
+void *t_function(void *data){
+    int i = (int)data;
 
-int main(){
-        int i;
-        int status;
-        int thr_id;
-        int str_size;
-        int difficulty = 3;
-
-        pthread_t p_thread;
-        thr_id = pthread_create(&p_thread, NULL, t_function, (void *)&difficulty);
-
-        if(thr_id < 0){
-                perror("thread create error : ");
-                exit(0);
-        }
-
-        pthread_join(p_thread, (void **)&status);
-
-        //printf("when it clear!\n");
-
-//      memset(data,0,sizeof(data));
-
-        /*if(argc!=2) {
-                printf("please input the parameter! \n");
-                printf("ex)./test_led 1234\n");
-                return -1;
-        }
-    str_size=(strlen(argv[1]));
-    if(str_size>MAX_DIGIT)
-    {
-        printf("Warning! 4 Digit number only!\n");
-        str_size=MAX_DIGIT;
+    unsigned char str[5] = {1, 1, 1, 1};
+    while(i>=0){
+        sprintf(str, "%d",i);
+        write(dev,str,4);
+        i--;
+        usleep(1000);
     }
-    for(i=0;i<str_size;i++)
-    {
-        if((argv[1][i]<0x30)||(argv[1][i])>0x39) {
-            printf("Error! Invalid Value!\n");
-            return -1;
-        }
-        data[i]=argv[1][i]-0x30;
-    }*/
-
-
-    /*if (dev<0) {
-        printf("Device open error : %s\n",FND_DEVICE);
-        exit(1);
-    }*/
-
-//      sleep(1);
-
-//      read(dev,&data,4);
-        /*if(retval<0) {
-                printf("Read Error!\n");
-                return -1;
-        }*/
-
-//      printf("Current FND Value : ");
-//      for(i=0;i<str_size;i++)
-//              printf("0x%x ",data[i]);
-        printf("\n");
-
-
-        return(0);
+    pthread_exit(NULL);
 }
