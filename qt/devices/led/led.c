0b04472466662ddea8f192f4917d3841369680d8
jinhak94 <47026766+jinhak94@users.noreply.github.com>
2019-12-18 16:52:46 +0900

    Add files via upload

--- qt/devices/led/led.c
+++ qt/devices/led/led.c
@@ -27,34 +27,24 @@ void led_destroy() {
 	close(dev);
 }
 
-pthread_t *led_thread(){
-     int thr_id;
-     int num;
-     pthread_t *p_thread = malloc(sizeof(pthread_t));
-     thr_id = pthread_create(p_thread, NULL, t_function, (void*)num);
-     if(thr_id<0){
-        perror("thread create error : ");
-        exit(1);
-     }
-     return &pthread;
-}
 
-void *t_function(void *data){
-    int i = 10;
-    data = 0; count = 0; temp = 0;
-    while(i > 0){
-        data = temp * 2 + 1;
-        if(data==511){
-            data = 0;
-            temp = 0;
-            count++;
-            if(count==3)
-                break;
-        }   
-        temp = data;
-        write(dev, *data, 1);
-        usleep(100000);
-    }
-    pthread_exit(NULL);
+void *led_function(void *d){
+        int i=10;
+
+        data = 0; count = 0; temp = 0;
+        while(i > 0){
+            data = temp * 2 + 1;
+            if(data == 511){
+                    data = 0;
+                    temp = 0;
+                    count++;
+                    if(count==3)
+                            break;
+
+            }
+            temp = data;
+            write(dev, &data, 1);
+            usleep(100000);
+        }
+        pthread_exit(NULL);
 }
-  
