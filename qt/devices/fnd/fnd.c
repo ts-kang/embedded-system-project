4bb5b7e044b2a15a847aef716ae7b1f6208da8de
jinhak94 <47026766+jinhak94@users.noreply.github.com>
2019-12-18 16:53:06 +0900

    Add files via upload

--- qt/devices/fnd/fnd.c
+++ qt/devices/fnd/fnd.c
@@ -1,49 +1,44 @@
+/* FPGA FND Test Application
+File : fpga_test_fnd.c*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <fcntl.h>
+#include <sys/types.h>
 #include <sys/stat.h>
-#include <err.h>
+#include <fcntl.h>
 #include <pthread.h>
-static int dev = -1;
-void fnd_init(){
-    dev = open("/dev/fpga_fnd",O_RDWR);
-    if(dev < 0)
-        printf("Asdfa");
+#include <string.h>
+#include <err.h>
+
+#define FND_DEVICE "/dev/fpga_fnd"
+
+void fnd_init() {
+	dev = open(LED_DEVICE, O_RDWR);
+	if (dev < 0) {
+		printf("push switch device open ERROR!!\n");
+		close(dev);
+		return -1;
+	}
 }
 
-void *t_function(void *data){
-    int i = *(int *)data;
-
-    unsigned char str[5] = {1, 1, 1, 1};
-    while(i>=0){
-        sprintf(str, "%d",i);
-        write(dev,str,4);
-        i--;
-       usleep(1000);
-    }
-    pthread_exit(NULL);
-}
-void fnd_destroy(){      
-    close(dev);
-}
-pthread_t *fnd_thread(int num){
-    int thr_id;
-    pthread_t *p_thread = malloc(sizeof(pthread_t));
-    thr_id = pthread_create(p_thread,NULL,t_function,(void *)&num);
-    if(thr_id < 0){
-        perror("thread create error : ");
-        exit(1);
-    }
-
-    return p_thread;
+void fnd_destroy() {
+	close(dev);
 }
 
+void *fnd_thread(void *data){
+       int* num = (int*)data;
+        int i = *num;
 
- 
-void main(){
- 
-    fnd_init();
-    fnd_thread(5000);
-    fnd_destroy();
+        
+
+        unsigned char str[5] = {1,1,1,1};
+  
+        while(i>=0){
+               sprintf(str, "%d",i);
+                write(dev,str,4);
+                i--;
+                usleep(1000);
+        }
+        pthread_exit(NULL);
 }
