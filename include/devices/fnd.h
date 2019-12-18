377078aa5b36d01ca57154a525331faf88b05220
jinhak94 <47026766+jinhak94@users.noreply.github.com>
2019-12-18 14:39:32 +0900

    Add files via upload

--- include/devices/fnd.h
+++ include/devices/fnd.h
@@ -1,7 +1,10 @@
-#ifndef _FND_H_
-#define _FND_H_
+
+#ifndef _LED_H_
+#define _LED_H_
 
 void fnd_init(void);
 void fnd_destroy(void);
 
-#endif /* _FND_H_ */
+void *t_function(void *);
+
+#endif /* _LED_H_ */
--- include/devices/led.h
+++ include/devices/led.h
@@ -1,7 +1,10 @@
+
 #ifndef _LED_H_
 #define _LED_H_
 
 void led_init(void);
 void led_destroy(void);
+
+void *t_function(void *);
 
 #endif /* _LED_H_ */
